8efb1c53bbab11ac1ef3c2447fa2ff2d521f6605
Minesh Parekh <saixyyu@gmail.com>
2024-02-23 10:02:12 +0000

    added basic readme and text changes

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# My-Portfolio
+# Portfolio - Min P.
+
+## Table of Contents
+
+* [Description](#description)
+* [Questions](#questions)
+* [License](#license)
+
+## Description
+
 A hub for all information about my work.
+
+## Questions
+
+ If you have any questions, contact me here: 
+
+* [GitHub Profile](https://github.com/minyyds)
+* Email: minhttps@outlook.com
+
+## License
+
+MIT
+
--- js/script.js
+++ js/script.js
@@ -9,12 +9,15 @@ function toggleTheme() {
 
     const moonIcon = document.querySelector('.moon-icon');
     const moonIconHover = document.querySelector(".theme-toggle")
+    const letsGo = document.querySelector(".proceed")
 
     if (document.body.classList.contains('light-mode')) {
         moonIcon.classList.add('light-mode');
         moonIconHover.classList.add("light-mode");
+        letsGo.classList.add("light-mode")
     } else {
         moonIcon.classList.remove('light-mode');
         moonIconHover.classList.remove("light-mode");
+        letsGo.classList.remove("light-mode")
     };
 };
