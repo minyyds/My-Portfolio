36d3bccd88d166e8e2faf4004b79310f134e1736
Minesh Parekh <saixyyu@gmail.com>
2024-02-23 07:57:50 +0000

    added code for text hero

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cursor = document.getElementById('cursor');
+    const nameElement = document.querySelector('.name');
+    nameElement.appendChild(cursor);
+});
