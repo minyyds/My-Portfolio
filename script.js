56f046619adaef406c33c53492fa931e71d3562f
Minesh Parekh <saixyyu@gmail.com>
2024-02-23 08:53:25 +0000

    added dark mode and light mode

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const nameElement = document.querySelector('.name');
     nameElement.appendChild(cursor);
 });
+
+function toggleTheme() {
+    document.body.classList.toggle('light-mode');
+
+    const moonIcon = document.querySelector('.moon-icon');
+
+    if (document.body.classList.contains('light-mode')) {
+        moonIcon.classList.add('light-mode');
+    } else {
+        moonIcon.classList.remove('light-mode');
+    };
+};
