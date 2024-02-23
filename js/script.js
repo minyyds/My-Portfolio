fcc7031c996937b5ec9d984b1f93f7c328d963cd
Minesh Parekh <saixyyu@gmail.com>
2024-02-23 16:16:29 +0000

    added text hover effect

--- js/script.js
+++ js/script.js
@@ -21,3 +21,27 @@ function toggleTheme() {
         letsGo.classList.remove("light-mode")
     };
 };
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+document.querySelector(".name").onmouseover = event => {
+    let iterations = 0
+
+    const interval = setInterval(() => {
+    event.target.innerText = event.target.innerText.split("")
+    .map((letter, index) => {
+        if(index < iterations) {
+            return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)]
+    })
+    .join("");
+
+    if(iterations >= event.target.dataset.value.length) {
+        clearInterval(interval);
+    }
+    iterations += 1 / 3;
+
+}, 30)
+}
