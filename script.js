69a711bbab4ac12294591db3fe0f425c993ba7cf
Minesh Parekh <saixyyu@gmail.com>
2024-02-23 09:14:52 +0000

    fixed issue with light mode button

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ function toggleTheme() {
     document.body.classList.toggle('light-mode');
 
     const moonIcon = document.querySelector('.moon-icon');
+    const moonIconHover = document.querySelector(".theme-toggle")
 
     if (document.body.classList.contains('light-mode')) {
         moonIcon.classList.add('light-mode');
+        moonIconHover.classList.add("light-mode");
     } else {
         moonIcon.classList.remove('light-mode');
+        moonIconHover.classList.remove("light-mode");
     };
 };
